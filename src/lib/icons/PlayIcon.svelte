b6ac6c0fa3647fbe7a6892aadf225f52a693aac2
Jason Zheng <jasonz4200@gmail.com>
2025-01-14 10:06:25 -0600

    Add icons

---
+++ src/lib/icons/PlayIcon.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  let { size = "medium" }: { size?: "small" | "medium" } = $props();
+
+  const styles = {
+    small: "h-5 w-5",
+    medium: "h-6 w-6",
+  }[size];
+</script>
+
+<svg
+  class="{styles} text-gray-800 dark:text-white"
+  aria-hidden="true"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  fill="none"
+  viewBox="0 0 24 24"
+>
+  <path
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M8 18V6l8 6-8 6Z"
+  />
+</svg>
